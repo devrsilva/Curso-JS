3e53430577821f793d0b51b4556ca5850340d863
Ryan <ryandias7519@gmail.com>
2025-06-18 12:03:44 -0300

    add sections, lesson content and practical exercises

---
+++ JavaScript/Seção 2/Class12/index.js
@@ -0,0 +1,14 @@
+//Valores primitivos e valores de referência
+/*Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol) - Valor copiados
+
+Referência (mutável) - array, object, function - Passados por referência
+*/
+// const a = {
+//   nome: "Ryan",
+//   sobrenome: "Silva",
+// };
+
+// const b = a;
+
+// a.nome = "João";
+// console.log(b);
